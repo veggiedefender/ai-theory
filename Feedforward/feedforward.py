8e372c0a2878cc335729d7cb02d6604f47fcdb13
veggiedefender <jessejesse123@gmail.com>
2017-12-19 20:50:02 -0500

    add docstrings

--- Feedforward/feedforward.py
+++ Feedforward/feedforward.py
@@ -1,6 +1,12 @@
+"""
+Implements XOR using a feedforward neural network
+"""
 import numpy as np
 
 class Neuron:
+    """
+    Neuron class that takes a layer of inputs, weights, and a bias
+    """
     def __init__(self, inputs, weights, bias):
         self.inputs = inputs
         self.weights = weights
@@ -8,13 +14,23 @@ class Neuron:
 
     @staticmethod
     def activation(x):
+        """
+        Calculates value of sigmoid function applied to a number
+        """
         return 1 / (1 + np.exp(-x))
 
     def resolve(self):
+        """
+        Resolve the value of a neuron
+        """
         inputs = [ neuron.resolve() for neuron in self.inputs ]
         return Neuron.activation(np.dot(inputs, self.weights) + self.bias)
 
-class InputNeuron:
+
+class InputNeuron(Neuron):
+    """
+    Special type of neuron that holds an input value and has no input layer
+    """
     def __init__(self, value):
         self.value = value
 
